a7cdb0a7f17b2a8efe4daa67c4cc839f1dc0dd06
whgkswo <gjuk7238@gmail.com>
2024-06-14 16:16:52 +0900

    commit

--- src/main/java/com/springboot/response/ErrorResponse.java
+++ src/main/java/com/springboot/response/ErrorResponse.java
@@ -1,7 +1,12 @@
 package com.springboot.response;
 
+import com.springboot.exception.BusinessLogicException;
+import com.springboot.exception.ExceptionCode;
 import lombok.Getter;
+import org.springframework.boot.web.reactive.error.ErrorWebExceptionHandler;
+import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 
 import javax.validation.ConstraintViolation;
 import java.util.List;
@@ -10,21 +15,43 @@ import java.util.stream.Collectors;
 
 @Getter
 public class ErrorResponse {
+    private int httpStatus;
+    private String message;
     private List<FieldError> fieldErrors;
     private List<ConstraintViolationError> violationErrors;
 
     private ErrorResponse(final List<FieldError> fieldErrors,
-                          final List<ConstraintViolationError> violationErrors) {
+                          final List<ConstraintViolationError> violationErrors,
+                          final Integer httpStatus,
+                          final String message) {
         this.fieldErrors = fieldErrors;
         this.violationErrors = violationErrors;
+        this.httpStatus = httpStatus;
+        this.message = message;
     }
 
     public static ErrorResponse of(BindingResult bindingResult) {
-        return new ErrorResponse(FieldError.of(bindingResult), null);
+        return new ErrorResponse(FieldError.of(bindingResult), null,null,null);
     }
 
     public static ErrorResponse of(Set<ConstraintViolation<?>> violations) {
-        return new ErrorResponse(null, ConstraintViolationError.of(violations));
+        return new ErrorResponse(null, ConstraintViolationError.of(violations), null,null);
+    }
+    public static ErrorResponse of(BusinessLogicException businessLogicException){
+        int httpStatus = businessLogicException.getExceptionCode().getStatus();
+        String message = businessLogicException.getMessage();
+        return new ErrorResponse(null,null, httpStatus,message);
+    }
+    public static ErrorResponse of(HttpRequestMethodNotSupportedException httpRequestMethodNotSupportedException){
+        HttpStatus httpStatus = HttpStatus.METHOD_NOT_ALLOWED;
+        return new ErrorResponse(null,null,httpStatus.value(),httpStatus.getReasonPhrase());
+    }
+    public static ErrorResponse of(NullPointerException nullPointerException){
+        HttpStatus httpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
+        return new ErrorResponse(null,null, httpStatus.value(), httpStatus.getReasonPhrase());
+    }
+    public static ErrorResponse of(String message, int httpStatus){
+        return new ErrorResponse(null,null,httpStatus,message);
     }
 
     @Getter
